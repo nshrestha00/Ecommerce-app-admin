455080360ee3d5c0f4b13b9ecbf19e19797accfd
Your Name <your.email@example.com>
2024-11-24 20:52:09 +0545

    files & folders structure created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import {assets} from "../assets/assets"
+
+const Navbar = () => {
+  return (
+    <div>
+        <img className="w-[max(10%, 80px)]" src={assets.logo} alt="" />
+        <button className="bg-gray-600 text-white px-4 py-2 sm:px-2 rounded-full text-xs sm:text-sm">Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
