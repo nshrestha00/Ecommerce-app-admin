370fced5dcf698976ee6bf8a0379e88b9515d3ca
Your Name <your.email@example.com>
2024-11-25 13:28:12 +0545

    Navbar and Sidebar file created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {assets} from "../assets/assets"
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex items-center py-2 pax-[4%] justify-between">
         <img className="w-[max(10%, 80px)]" src={assets.logo} alt="" />
         <button className="bg-gray-600 text-white px-4 py-2 sm:px-2 rounded-full text-xs sm:text-sm">Logout</button>
     </div>
