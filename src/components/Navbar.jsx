39ef9f5a14d5c50638a47d5a02b9b9c1e48a7d4c
Your Name <your.email@example.com>
2024-11-26 12:23:40 +0545

    Fix: Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import {assets} from "../assets/assets"
+import { assets } from "../assets/assets";
 
-const Navbar = ({setToken}) => {
+const Navbar = ({ setToken }) => {
   return (
-    <div className="flex items-center py-2 pax-[4%] justify-between">
-        <img className="w-[max(10%, 80px)]" src={assets.logo} alt="" />
-        <button onClick={()=>setToken("")} className="bg-gray-600 text-white px-4 py-2 sm:px-2 rounded-full text-xs sm:text-sm">Logout</button>
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <img className="w-20 sm:w-32" src={assets.logo} alt="Logo" />
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-4 py-2 rounded-full text-sm"
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
