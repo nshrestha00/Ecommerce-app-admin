34abbdbd5d78fa2c970f4a6102cad96829eb6ca0
Your Name <your.email@example.com>
2025-02-23 14:11:08 +0545

    Added navigation function in logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { assets } from "../assets/assets";
 
 const Navbar = ({ setToken }) => {
+  const navigate = useNavigate(); 
+
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img className="w-20 sm:w-32" src={assets.logo} alt="Logo" />
+      <img
+        className="w-20 sm:w-32 cursor-pointer" 
+        src={assets.logo}
+        alt="Logo"
+        onClick={() => navigate('/')}
+      />
       <button
         onClick={() => setToken("")}
         className="bg-gray-600 text-white px-4 py-2 rounded-full text-sm"
